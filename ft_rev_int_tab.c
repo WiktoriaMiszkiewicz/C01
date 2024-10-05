4392c94e6ea7485b593b3901e65a8dfa4cce4195
Wiktoria <87610555+WiktoriaMiszkiewicz@users.noreply.github.com>
2024-10-05 13:47:57 +0200

    Add files via upload

--- ft_rev_int_tab.c
+++ ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: wimiszki <wimiszki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 15:39:00 by wiktoria          #+#    #+#             */
-/*   Updated: 2024/10/05 13:42:44 by wimiszki         ###   ########.fr       */
+/*   Updated: 2024/10/05 13:47:41 by wimiszki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,26 @@
 
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	reversed_tab[size];
 	int	i;
+	int	temp;
 
 	i = 0;
-	while (i < size)
+	while (i < size / 2)
 	{
-		reversed_tab[i] = tab[size - 1 - i];
-		i++;
-	}
-	i = 0;
-	while (i < size)
-	{
-		tab[i] = reversed_tab[i];
+		temp = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = temp;
 		i++;
 	}
 }
 
 int	main(void)
 {
-	int tab[] = {1, 2, 3, 4, 7}; // Przykładowa tablica
-	int size;
-	int i;
+	int	tab[] = {1, 2, 3, 4, 5};  // Przykładowa tablica
+	int	size;
+	int	i;
 
-	size = sizeof(tab) / sizeof(tab[0]); // Obliczanie rozmiaru tablicy
+	size = sizeof(tab) / sizeof(tab[0]);  // Obliczanie rozmiaru tablicy
 
 	printf("Oryginalna tablica: ");
 	for (i = 0; i < size; i++)
@@ -46,7 +42,7 @@ int	main(void)
 	}
 	printf("\n");
 
-	ft_rev_int_tab(tab, size); // Odwracanie tablicy
+	ft_rev_int_tab(tab, size);  // Odwracanie tablicy
 
 	printf("Odwrócona tablica: ");
 	for (i = 0; i < size; i++)
